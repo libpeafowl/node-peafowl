c0f02e596157ba0131ec38d9a0f26805967fcfc5
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-08-03 17:41:34 +0200

    extend test debug

--- peafowl.js
+++ peafowl.js
@@ -1,7 +1,10 @@
 var bindings = require('node-gyp-build')(__dirname)
 
-console.log(bindings.times_two(42))
+console.log(bindings.test_mul(42))
+
 module.exports = {
-  init: function(){ return; },
-  times_two: bindings.times_two
+  times_two: bindings.test_mul, // Params: INT
+  pfw_init: bindings.pfw_init, // Params: INT (1 stateful, 0 stateless)
+  pfw_get_protocol: bindings.pfw_get_protocol, // Params: struct pcap_pkthdr *, header
+  pfw_terminate: bindings.pfw_terminate // Params: VOID
 }
